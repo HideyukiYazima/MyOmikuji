0184bc30b0cb314e58eeb1e4cd3d825082ac0ad4
Yazima <yazima04200@gmail.com>
2023-01-13 23:47:23 +0900

    完成

--- main.js
+++ main.js
@@ -5,6 +5,23 @@
   const result = document.getElementById('result');
 
   btn.addEventListener('click', () => {
-    result.textContent = 'hit!';
-  })
+    const results = ['大吉', '中吉', '凶', '小吉'];
+    const n = Math.floor(Math.random() * results.length);
+
+    result.textContent = results[n];
+
+    /* スイッチ文は上記の通り短縮できるので使用しない
+    switch (n) {
+      case 0:
+      result.textContent = '大吉';
+      break; // switch文にbreakは超重要
+      case 1:
+      result.textContent = '中吉';
+      break;
+      case 2:
+      result.textContent = '凶';
+      break;
+    }
+    */
+  });
 }
